19b6a313059de4559209178341df5ab9ef50e247
Gary Bryden <gazbot@gmail.com>
2015-11-16 12:40:43 +1100

    fixed gdisconnect

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -176,13 +176,12 @@ def getUserID(email):
 @app.route('/gdisconnect')
 def gdisconnect():
         # Only disconnect a connected user.
-    credentials = login_session.get('credentials')
     if 'username' not in login_session:
         response = make_response(
             json.dumps('Current user not connected.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-    access_token = credentials.access_token
+    access_token = login_session.get('credentials')
     url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
